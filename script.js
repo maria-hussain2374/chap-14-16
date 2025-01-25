c505feea46de3cd66d6044899515aa4fa76d2317
Maria <amnaattaria325@gmail.com>
2025-01-25 16:20:21 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,117 @@
+//question 1
+let emptyarr=[];
+//question 2
+// let emptyobj=newarrray();
+// //question 3
+// let str=["book","copy","pencil","marker"];
+// //question4
+// let num=[12,15,16,17,18];
+// //question 5
+// let boolean=[true,false,false,true];
+// //question 6
+// let mixedarr=[null,undefined,12,true,false,"maria"];
+//question 7
+// document.write("QUALIFICATION:","<br>","<br>","<br>");
+// let qualifications=["SSC","HSC","BCS","BS","BCOM","MS","M.PHILL","PHD"]
+// for(let i=0; i<qualifications.length;i++){
+//     document.write(`${i+1})${qualifications[i]}`);
+//     document.write("<br>")
+// }
+
+//question 8
+// let student=["summaiya","huma","hira"];
+// let score=[67,78,99];
+// document.write(`score of summaiya is ${score[0]} and percentage is 64%`,"<br>");
+// document.write(`score of summaiya is ${score[1]} and percentage is 73%`,"<br>");
+// document.write(`score of summaiya is ${score[2]} and percentage is 90%`,"<br>");
+
+//question 9
+let colorname=["red","green","blue","yellow","brown",'white'];
+// let optiona=prompt("please enter which colour do you want to add at start?");
+// colorname.unshift(optiona);
+// console.log(colorname);
+
+//add favourite color at the end
+// let optionb=prompt("please enter which colour do you want to add at the end?");
+// colorname.push(optionb);
+// console.log(colorname);
+
+
+//add 2 more array at start
+// colorname.unshift("skyblue","aquamarine");
+// console.log(colorname);
+
+//delete the first array
+// colorname.shift();
+// console.log(colorname);
+
+//delete the last color
+
+// colorname.pop();
+// console.log(colorname);
+
+//add new color at index
+// let user=prompt("at which index do you want to add color?");
+// let newcolor=prompt("now tell me new color of your choice")
+
+// colorname.splice(user,0,newcolor);
+// console.log(colorname);
+
+
+//delete the array
+// let user1=prompt("at which index do you want to delete?");
+// let deletecolor=prompt("which color you want to delete");
+
+// colorname.splice(0,3);
+// console.log(colorname);
+
+
+//question 10
+// let studentscore=[320,230,480,120];
+// console.log("score of students",studentscore);
+// let c=studentscore.sort();
+// console.log("ordered score oF students",c);
+
+
+// question 11
+// let citieslist=["karachi","lahore","islamabad","quetta","peshawar"];
+// console.log("cities list");
+// let d=citieslist.slice(2,4);
+// console.log("selected citieslist",d);
+
+
+//question 12
+
+// let arr=["This","is","a","cat"];
+// let str=arr.join( "  ");
+// console.log(str)
+
+
+//question 13
+
+//FIFO METHOD
+// let array=["keyboard","mouse","printer","monitor"];
+// console.log("DEVICES:\n",array);
+// console.log("out:\n","keyboard");
+// console.log("out:\n","mouse");
+// console.log("out:\n","printer");
+// console.log("out:\n","monitor");
+
+//question 14
+// //LIFO METHOD
+// let array2=["keyboard","mouse","printer","monitor"];
+// console.log("DEVICES:\n",array);
+// console.log("out:\n","monitor");
+// console.log("out:\n","printer");
+// console.log("out:\n","mouse");
+// console.log("out:\n","keyboard");
+
+//question 15
+
+// let phone=["APPLE","SAMSUNG","MOTOROLA","NOKIA","SONY","HAIER"];
+// document.write('<select>');
+
+// for(let i=0;i<phone.length;i++){
+//     document.write("<option>"+ phone[i] +"</option>")
+// }   
+//     document.write("</select")
